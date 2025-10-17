98ae7ff7ac78c4d6da02df863466f605ce9f0622
andgan <andresganga1298@gmail.com>
2025-10-18 00:50:49 +0200

    creacion primeros endpoints

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('node:http')
+const express = require('express')
 const cors = require('cors')
+const app = express()
 
 const ingresos = require('./ingresos.json')
 const gastos = require('./gastos.json')
@@ -11,52 +13,66 @@ const corsMiddleware = cors({
   allowedHeaders: ['Content-Type']
 })
 
+
+//Endpoint para obtener todos los ingresos
+app.get('/ingresos', (req, res) => {
+  res.status(200).json(ingresos);
+});
+
+// Endpoint para obtener ingresos filtrados por mes y año, considerando que la fecha es un string en formato "YYYY-MM-DD" y que se pasa como parametro enviado en la url
+app.get('/ingresos/:anyo/:mes', (req, res) => {
+  const { anyo, mes } = req.params;
+  if (mes && anyo) {
+    console.log(mes, anyo);
+    const ingresosFiltrados = ingresos.filter(ingreso => {
+      const fecha = new Date(ingreso.fecha);
+      return fecha.getMonth() + 1 === parseInt(mes) && fecha.getFullYear() === parseInt(anyo);
+    });
+    res.status(200).json(ingresosFiltrados);
+  } else {
+    // res.status(200).json(ingresos);
+    res.status(404).json({ error: 'Parámetros mes y año no han sido encontrados' });
+  }
+});
+
+// Endpoint para craear un nuevo ingreso
+app.post('/ingresos', (req, res) => {
+  const nuevoIngreso = req.body;
+  ingresos.push(nuevoIngreso);
+  res.status(201).json(nuevoIngreso);
+});
+
+
+//Endpoint para obtener todos los gastos
+app.get('/gastos', (req, res) => {
+  res.json(gastos);
+});
+
+// Endpoint para obtener gastos filtrados por mes y año, considerando que la fecha es un string en formato "YYYY-MM-DD" y que se pasa como parametro enviado en la url
+app.get('/gastos/:anyo/:mes', (req, res) => {
+  const { anyo, mes } = req.params; 
+
+  if (mes && anyo) {
+    const gastosFiltrados = gastos.filter(gasto => {
+      const fecha = new Date(gasto.fecha);
+      return fecha.getMonth() + 1 === parseInt(mes) && fecha.getFullYear() === parseInt(anyo);
+    });
+    res.status(200).json(gastosFiltrados);
+  } else {
+    res.status(404).json({ error: 'Parámetros mes y año no han sido encontrados' });
+  }
+}); 
+
+// Endpoint para craear un nuevo gasto
+app.post('/gastos', (req, res) => {
+  const nuevoGasto = req.body;
+  gastos.push(nuevoGasto);
+  res.status(201).json(nuevoGasto);
+}); 
+
 const processRequest = (req, res) => {
-  // Aplicar middleware CORS
   corsMiddleware(req, res, () => {
-    const { method, url } = req
-    console.log(method, url)
-
-    switch (method) {
-      case 'GET':
-        switch (url) {
-          case '/ingresos':
-            res.setHeader('Content-Type', 'application/json; charset=utf-8')
-            return res.end(JSON.stringify(ingresos))
-          case '/gastos':
-            res.setHeader('Content-Type', 'application/json; charset=utf-8')
-            return res.end(JSON.stringify(gastos))
-          default:
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'text/html; charset=utf-8')
-            return res.end('<h1>404</h1>')
-        }
-
-      case 'POST':
-        switch (url) {
-          case '/ingresos': {
-            let body = ''
-
-            req.on('data', chunk => {
-              body += chunk.toString()
-            })
-
-            req.on('end', () => {
-              const data = JSON.parse(body)
-              res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
-              data.timestamp = Date.now()
-              res.end(JSON.stringify(data))
-            })
-
-            break
-          }
-
-          default:
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-            return res.end('404 Not Found')
-        }
-    }
+    app(req, res)
   })
 }
 
@@ -64,4 +80,4 @@ const server = http.createServer(processRequest)
 
 server.listen(1234, () => {
   console.log('server listening on port http://localhost:1234')
-})
+})  
