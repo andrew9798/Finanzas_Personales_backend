666758596373a922fde540203bb99861544fe32f
andgan <andresganga1298@gmail.com>
2025-11-03 15:18:59 +0100

    changes pool connection.js

--- data/Connection.js
+++ data/Connection.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'tu_usuario',
-  password: 'tu_contraseña',
-  database: 'nombre_de_tu_bd',
+  user: 'root',
+  password: '',
+  database: 'FinanzasPersonales',
 });
 
 export default pool;
