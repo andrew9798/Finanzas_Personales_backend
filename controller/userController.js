4684f34dc335f01328fc2db4e0d147f213b697e3
andgan <andresganga1298@gmail.com>
2026-02-14 13:06:00 +0100

    create userController and all get method

---
+++ controller/userController.js
@@ -0,0 +1,49 @@
+import TransaccionesModel from '../models/transaccionesModel.js';
+import { randomUUID } from 'crypto';
+
+export default class TransaccionesController {
+    static async getAll(req, res) {
+        try {
+            const tipo = req.tipo;
+            const transacciones = await TransaccionesModel.getAll(tipo);
+            res.status(200).json(transacciones);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getById(req, res) {
+        try {
+            const tipo = req.tipo;
+            const { id } = req.params;
+            const transaccion = await TransaccionesModel.getById(tipo, id);
+            res.status(200).json(transaccion);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getByEmail(req, res) {
+        try {
+            const tipo = req.tipo;
+            const { email } = req.params;
+            const transaccion = await TransaccionesModel.getByEmail(tipo, email);
+            res.status(200).json(transaccion);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }   
+    }
+
+    static async getByNickname(req, res) {
+        try {
+            const tipo = req.tipo;
+            const { nickname } = req.params;
+            const transaccion = await TransaccionesModel.getByNickname(tipo, nickname);
+            res.status(200).json(transaccion);
+        } catch (error) {{
+            res.status(500).json({ error: error.message });
+        }
+    }
+}
+
+}
