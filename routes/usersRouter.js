e499173d1661afd60a8e6862edb8be174fe8012c
andgan <andresganga1298@gmail.com>
2026-02-19 01:05:27 +0100

    create userRouter.js

---
+++ routes/usersRouter.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import userController from "../controller/userController.js";
+
+export const usersRouter = Router();
+
+// Endpoint para obtener todos los usuarios
+usersRouter.get('/', userController.getAll);
+
+// Endpoint para obtener un usuario por ID
+usersRouter.get('/:id', userController.getById);
+
+// Endpoint para obtener un usuario por email
+usersRouter.get('/email/:email', userController.getByEmail);
+
+// Endpoint para obtener un usuario por nickname
+usersRouter.get('/nickname/:nickname', userController.getByNickname);
+
+// Endpoint para crear un nuevo usuario
+usersRouter.post('/', userController.create);
+
+// Endpoint para actualizar un usuario existente
+usersRouter.put('/:id', userController.update);
+
+// Endpoint para eliminar un usuario
+usersRouter.delete('/:id', userController.delete);
+
+export default usersRouter;
